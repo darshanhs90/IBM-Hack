db2a69268b0ba5d869dcbefc41c2443ecae79780
Haridarshan HS <hsdars@gmail.com>
2015-06-23 12:17:08 -0500

    map module added

--- HTML setup/js/studentCompanyInsights.js
+++ HTML setup/js/studentCompanyInsights.js
@@ -40,6 +40,36 @@ $http({
     params: {company:$scope.company}
  }).success(function(data, status, headers, config) {
     alert(data);
+    // map
+ var map = new google.maps.Map(document.getElementById('map'), { 
+       mapTypeId: google.maps.MapTypeId.TERRAIN,
+       zoom: 12
+   });
+
+   var geocoder = new google.maps.Geocoder();
+
+   geocoder.geocode({
+      'address': address
+   }, 
+   function(results, status) {
+      if(status == google.maps.GeocoderStatus.OK) {
+         new google.maps.Marker({
+            position: results[0].geometry.location,
+            map: map
+         });
+         map.setCenter(results[0].geometry.location);
+      }
+      else {
+         // Google couldn't geocode this request. Handle appropriately.
+      }
+   });
+// map ends
+
+
+
+
+
+
  });
 }
 
